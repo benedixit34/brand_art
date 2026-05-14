85d2cd1e2c46c1a3056382531d5899e68e502a0c
Benedixit34 <luciusemuh@gmail.com>
2026-05-14 19:10:27 +0100

    Update the Hero Player

--- components/HeroPlayer.tsx
+++ components/HeroPlayer.tsx
@@ -1,31 +1,37 @@
+"use client";
+
 import ReactPlayer from "react-player";
 
 export default function HeroVideo() {
   return (
-    <div className="w-screen h-screen overflow-hidden">
+    <div className="relative w-screen h-screen overflow-hidden">
+       <div
+        className="
+          absolute top-1/2 left-1/2
+          -translate-x-1/2 -translate-y-1/2
+          w-[177.78vh] h-[56.25vw]
+          min-w-full min-h-full
+        "
+      >
       <ReactPlayer
         src="https://vimeo.com/1190493402"
         playing
         muted
         loop
         controls={false}
-        width="100vw"
-        height="56.25vw"
+        width="100%"
+        height="100%"
         config={{
           vimeo: {
             background: true,
           },
         }}
-          style={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            minWidth: "100%",
-            minHeight: "100%",
-            objectFit: "cover"
-          }}
+        style={{
+          position: "absolute",
+          inset: 0,
+        }}
       />
+      </div>
     </div>
   );
 }
